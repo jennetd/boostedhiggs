597658f6df47030a95fd10520aec6e81e31a35a2
Jennet Dickinson <dickinson.jennet@gmail.com>
2021-03-24 15:49:27 -0500

    master

--- boostedhiggs/__init__.py
+++ boostedhiggs/__init__.py
@@ -1,23 +1,5 @@
 from .version import __version__
-from .hbbprocessor import HbbProcessor
-from .hbblowmuprocessor import HbbLowMuPtProcessor
-from .hbbloosetauprocessor import HbbLooseTauProcessor
-from .vbfprocessor import VBFProcessor
-from .vh1dprocessor import VH1DProcessor
-from .vh1dcprocessor import VH1DcProcessor
-from .vhprocessor import VHProcessor
-from .vbfddbopt import VBFddbProcessor
-from .btag import BTagEfficiency
 
 __all__ = [
     '__version__',
-    'HbbProcessor',
-    'HbbLowMuPtProcessor',
-    'HbbLooseTauProcessor',
-    'VBFProcessor',
-    'VH1DProcessor',
-    'VH1DcProcessor',
-    'VHProcessor',
-    'VBFddbProcessor',
-    'BTagEfficiency',
 ]
