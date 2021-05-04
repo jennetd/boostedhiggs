991c9f5a6332ae52bfdf50a21a9007201d9f7d41
Jennet Dickinson <dickinson.jennet@gmail.com>
2021-05-04 10:29:29 -0500

    inclusive notebooks

--- boostedhiggs/__init__.py
+++ boostedhiggs/__init__.py
@@ -1,11 +1,9 @@
 from .version import __version__
 from .hbbprocessor import HbbProcessor
-from .vbfprocessor import VBFProcessor
 from .btag import BTagEfficiency
 
 __all__ = [
     '__version__',
     'HbbProcessor',
-    'VBFProcessor',
     'BTagEfficiency',
 ]
