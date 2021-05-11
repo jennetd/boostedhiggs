79b2e35afb2e383abf7f25d7776c04538d858aeb
Jennet Dickinson <dickinson.jennet@gmail.com>
2021-05-11 17:03:59 -0500

    implement pickle

--- boostedhiggs/__init__.py
+++ boostedhiggs/__init__.py
@@ -1,11 +1,15 @@
 from .version import __version__
 from .hbbprocessor import HbbProcessor
 from .vbfprocessor import VBFProcessor
+from .vbfmuon import VBFmuProcessor
+from .vbfddbopt import VBFddbProcessor
 from .btag import BTagEfficiency
 
 __all__ = [
     '__version__',
     'HbbProcessor',
     'VBFProcessor',
+    'VBFmuProcessor',
+    'VBFddbProcessor',
     'BTagEfficiency',
 ]
