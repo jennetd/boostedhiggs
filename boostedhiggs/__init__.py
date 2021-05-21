e02889fcf2ad3ff5ddb562ceee19c548de729783
Jennet Dickinson <dickinson.jennet@gmail.com>
2021-05-21 14:51:40 -0500

    second binder

--- boostedhiggs/__init__.py
+++ boostedhiggs/__init__.py
@@ -1,9 +1,11 @@
 from .version import __version__
 from .hbbprocessor import HbbProcessor
+from .hbbmuon import HbbMuProcessor
 from .btag import BTagEfficiency
 
 __all__ = [
     '__version__',
     'HbbProcessor',
+    'HbbMuProcessor',
     'BTagEfficiency',
 ]
